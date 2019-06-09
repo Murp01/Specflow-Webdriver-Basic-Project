e6728e3b8efe87b3d0eac7222be9894cf1f48443
Murp01 <pmurphy35@hotmail.com>
2019-06-09 16:31:27 +0100

    Completed Practice Exercise 02.  This exercise opens a webpage and clicks on an element on the page

---
+++ Specflow Webdriver Training Project 01/Specflow Webdriver Training Project 01/Selenium Basics/PracticeExercise02.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Specflow_Webdriver_Training_Project_01.Selenium_Basics
+{
+    class PracticeExercise02
+    {
+        IWebDriver driver;
+
+        [Test]
+        public void PracticeExercise_02()
+        {
+            //1. Open Firefox driver
+            driver = new FirefoxDriver();
+
+            //2. Open webpage in browser
+            driver.Url = "https://demoqa.com/";
+
+            //3. Click on element on webpage
+            driver.FindElement(By.XPath("//a[contains(text(),'Selectable')]")).Click();
+           
+            //4. Close browser
+            driver.Quit();
+        }
+
+    }
+}
