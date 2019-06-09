29d6b9e39073794a66dce46457256618954a6bec
Murp01 <pmurphy35@hotmail.com>
2019-06-09 23:03:24 +0100

    Completed Practice Exercise04 - Send keys to form then click submit button

---
+++ Specflow Webdriver Training Project 01/Specflow Webdriver Training Project 01/Selenium Basics/PracticeExercise04.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Specflow_Webdriver_Training_Project_01.Selenium_Basics
+{
+    class PracticeExercise04
+    {
+
+        IWebDriver driver;
+
+        [Test]
+        public void WebElementClick()
+        {           
+            driver = new FirefoxDriver();
+            driver.Url = "https://www.toolsqa.com/Automation-practice-form/";
+
+            IWebElement InputBox_FirstName = driver.FindElement(By.XPath("//input[contains(@name,'firstname')]"));
+            IWebElement Button_Submit = driver.FindElement(By.XPath("//button[@id='submit']"));
+
+            InputBox_FirstName.SendKeys("Paul");
+            Button_Submit.Click();
+        }
+
+
+    }
+}
