75494b2a174cde617d0fb1f3ae19e677c2fce54a
Murp01 <pmurphy35@hotmail.com>
2019-06-09 11:35:47 +0100

    Created 2 classes - 1 simple test, second class contains NUnit test.  NUnit is saved as extension so others will have to install extension

---
+++ Specflow Webdriver Training Project 01/Specflow Webdriver Training Project 01/NUnitTest.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Specflow_Webdriver_Training_Project_01
+{
+    class NUnitTest
+
+    {
+        IWebDriver driver;
+
+        [SetUp]
+        public void initialize()
+        {
+            driver = new FirefoxDriver();
+        }
+
+        [Test]
+        public void OpenAppTest()
+        {
+            driver.Url = "http://www.demoqa.com";          
+        }
+
+        [TearDown]
+        public void EndTest()
+        {
+            driver.Close();
+        }
+    }
+}
