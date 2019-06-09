e9e9a6a10f3ec4c6687bb0a5f85127e9fc14e351
Murp01 <pmurphy35@hotmail.com>
2019-06-09 11:10:22 +0100

    Webdrivers added - note: Added the coded IWebDriver and installed from red squiggly.  Installed firefox from Tools > Manage

--- Specflow Webdriver Training Project 01/Specflow Webdriver Training Project 01/FirstTestCase.cs
+++ Specflow Webdriver Training Project 01/Specflow Webdriver Training Project 01/FirstTestCase.cs
@@ -1,15 +1,20 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Specflow_Webdriver_Training_Project_01
 {
-    class Program
+    class FirstTestCase
     {
         static void Main(string[] args)
         {
+            IWebDriver driver = new FirefoxDriver();
+            driver.Url = "http://www.demoqa.com";
         }
     }
 }
