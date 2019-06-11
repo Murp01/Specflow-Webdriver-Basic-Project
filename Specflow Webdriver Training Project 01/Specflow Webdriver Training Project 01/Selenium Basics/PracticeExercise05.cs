72faa5dcf31d28603f83f3f07cc82c358dcf707e
Murp01 <pmurphy35@hotmail.com>
2019-06-11 21:59:07 +0100

    Completed link text exercise

---
+++ Specflow Webdriver Training Project 01/Specflow Webdriver Training Project 01/Selenium Basics/PracticeExercise05.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Specflow_Webdriver_Training_Project_01.Selenium_Basics
+{
+    class PracticeExercise05
+    {
+        IWebDriver driver;       
+
+        //[Test]
+        public void ClickVariousElementMethods()
+        {
+            driver = new FirefoxDriver();
+
+            driver.Url = "http://toolsqa.com/automation-practice-form/";
+
+            driver.FindElement(By.Name("firstname")).SendKeys("Alex");
+            driver.FindElement(By.Name("lastname")).SendKeys("Murphy");
+            driver.FindElement(By.Id("submit")).Click();
+            driver.Quit();
+
+        }
+
+        [Test]
+        public void ClickOnLinkWithPartialText()
+        {
+            driver = new FirefoxDriver();
+            driver.Url = "http://toolsqa.com/automation-practice-form/";
+
+            driver.FindElement(By.PartialLinkText("Partial Li")).Click();
+            String SubmitButtonTag = driver.FindElement(By.TagName("button")).ToString();
+            Console.WriteLine(SubmitButtonTag);
+            driver.FindElement(By.LinkText("Link Test")).Click();
+            Console.WriteLine("Link Test Pass");
+            driver.Quit();
+
+        }
+    }
+}
